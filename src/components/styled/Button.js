11500b422ce27a861619dfea1f63738597d48a37
clalil <cliljander@gmail.com>
2021-05-02 17:06:14 +0200

    Basic css setup, import font

--- src/components/styled/Button.js
+++ src/components/styled/Button.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const PrimaryBtn = styled.button`
 display: inline-block;
+font-family: 'Nunito Sans', sans-serif;
 border-radius: 10px;
 padding: 0.5rem 0;
 margin: 0.5rem 1rem;
@@ -10,7 +11,7 @@ width: 11rem;
 background: linear-gradient(
   20deg
   , rgb(33, 33, 33), rgb(66, 66, 66));
-color: white;
+color: var(--main-color);
 border: 2px solid white;
 `
 
