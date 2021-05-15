c9dbc41f50d1e8e1c7dcef45b9176e223efc724b
clalil <cliljander@gmail.com>
2021-05-15 15:58:53 +0200

    Finish pinned projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-const Image = styled.div `
+const Circle = styled.div `
+position: relative;
+display: block;
 background: ${props => `url(${props.background}) top center / cover no-repeat`};
-color: white;
 height: 200px;
 width: 200px;
 align-self: center;
-background-color: white;
+color: var(--white);
+background-color: var(--black);
 border-radius: 100%;
 opacity: 0.8;
 transition: all 1.2s ease;
+border: 5px solid var(--light-border-color);
 box-shadow: 1px 2px 10px 0px rgba(0,0,0,0.2);
 
 &:hover {
@@ -23,15 +26,72 @@ box-shadow: 1px 2px 10px 0px rgba(0,0,0,0.2);
 }
 `
 
+const Content = styled.div `
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+  background-color: rgba(0, 0, 0, 0.50);
+`
+
+const Wrapper = styled.div `
+display: table;
+width: 100%;
+height: 100%;
+` 
+
+const TextWrapper = styled.div `
+display: table-cell;
+padding: 1em;
+vertical-align: middle;
+`
+
+const H2 = styled.h2 `
+  font-weight: 700;
+  line-height: 1em;
+  font-size: 1.2rem !important;
+`
+
+const P = styled.p `
+  font-weight: 300;
+  font-size: 0.8rem !important;
+  margin-bottom: 5px;
+`
+
+const Link = styled.a `
+  display: flex;
+`
+
+const Image = styled.img `
+  margin: auto;
+  height: 30px;
+  width: 30px;
+  //Barrett Sonntag filter calculator used to calculate hue
+  filter: invert(99%) sepia(1%) saturate(197%) hue-rotate(25deg) brightness(115%) contrast(92%);
+
+  :hover {
+    filter: invert(58%) sepia(8%) saturate(1179%) hue-rotate(298deg) brightness(96%) contrast(91%);
+  }
+`
+
 export const Card = ({ project }) => {
   return (
     <>
-      <Image background={`/img/${project.repo}.jpg`}>
-          <p>name={project.repo}</p>
-          <p>description={project.description}</p> 
-          <p>language={project.language}</p> 
-          <p>link={project.link}</p>
-      </Image>
+      <Circle background={`/img/${project.repo}.jpg`}>
+        <Content>
+          <Wrapper>
+            <TextWrapper>
+              <H2>{project.description}</H2> 
+              <P>#{project.language}</P> 
+              <Link href={project.link} target="_blank">
+                  <Image src="/img/gh.png" />
+              </Link>
+            </TextWrapper>
+          </Wrapper>
+        </Content>
+      </Circle>
     </>
   )
 }
